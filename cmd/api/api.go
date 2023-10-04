047e25405568879ea3f27ab659c8215b2ad1800e
Vikash Parashar <vikash@nexg.tech>
2023-10-04 17:16:19 +0530

    initial commit

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -11,12 +10,10 @@ import (
 )
 
 const version = "1.0.0"
-const cssVersion = "1"
 
 type config struct {
 	port int
 	env  string
-	api  string
 	db   struct {
 		dsn string
 	}
@@ -27,11 +24,10 @@ type config struct {
 }
 
 type application struct {
-	config        config
-	infoLog       *log.Logger
-	errorLog      *log.Logger
-	templateCache map[string]*template.Template
-	version       string
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	version  string
 }
 
 func (app *application) Server() error {
@@ -44,35 +40,31 @@ func (app *application) Server() error {
 		WriteTimeout:      5 * time.Second,
 	}
 
-	app.infoLog.Printf("starting HTTP server in %s mode on port %d", app.config.env, app.config.port)
+	app.infoLog.Printf("Starting Backend server in %s mode on port %d", app.config.env, app.config.port)
 	return srv.ListenAndServe()
 }
-
 func main() {
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4000, "server port to listen")
-	flag.StringVar(&cfg.env, "env", "development", "Application Environment {development|production}")
-	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api's")
+	flag.IntVar(&cfg.port, "port", 4001, "server port to listen")
+	flag.StringVar(&cfg.env, "env", "development", "Application Environment {development|production|maintenance}")
+
 	flag.Parse()
+
 	cfg.stripe.key = os.Getenv("STRIPE_KEY")
 	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
 	infoLog := log.New(os.Stdout, "INFO : \t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR : \t", log.Ldate|log.Ltime|log.Lshortfile)
-	tc := make(map[string]*template.Template)
 
 	app := &application{
-		config:        cfg,
-		infoLog:       infoLog,
-		errorLog:      errorLog,
-		templateCache: tc,
-		version:       version,
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		version:  version,
 	}
 
 	err := app.Server()
 	if err != nil {
-		app.errorLog.Println(err)
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
-
 }
